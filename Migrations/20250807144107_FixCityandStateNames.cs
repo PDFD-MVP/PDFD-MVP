09b03f5968a4dfb82a84b32ae42b5c76ed32b187
PBFD-MVP <PBFD.MVP@gmail.com>
2025-08-07 10:44:33 -0400

    Fixed city and state names

---
+++ Migrations/20250807144107_FixCityandStateNames.cs
@@ -0,0 +1,46 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace VisitorLog_PDFD.Migrations
+{
+    /// <inheritdoc />
+    public partial class FixCityandStateNames : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.UpdateData(
+                table: "Counties",
+                keyColumn: "CountyId",
+                keyValue: 2,
+                column: "Name",
+                value: "Baltimore");
+
+            migrationBuilder.UpdateData(
+                table: "States",
+                keyColumn: "StateId",
+                keyValue: 2,
+                column: "Name",
+                value: "Virginia");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.UpdateData(
+                table: "Counties",
+                keyColumn: "CountyId",
+                keyValue: 2,
+                column: "Name",
+                value: "Boltimore");
+
+            migrationBuilder.UpdateData(
+                table: "States",
+                keyColumn: "StateId",
+                keyValue: 2,
+                column: "Name",
+                value: "Viginia");
+        }
+    }
+}
